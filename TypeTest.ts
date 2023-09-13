37b9b1edede27b79365b664fc8d7ebac794470b3
M153 <wanchatgame@gmail.com>
2023-09-13 19:30:01 +0700

    Variable & Data Types

---
+++ TypeTest.ts
@@ -0,0 +1,16 @@
+var myName: string = "M";
+console.log(myName);
+console.log(typeof(myName));
+
+var money: number = 100;
+console.log(money);
+console.log(typeof(money));
+
+console.log(null === undefined) //false
+console.log(null == undefined) //true
+console.log(null === null) //true
+
+// var name:string = "?"
+// var age: number;
+// var canVote = true;
+// var i;
