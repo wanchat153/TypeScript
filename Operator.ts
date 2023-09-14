80b1b2cbbcca19bfcd8c02b6840fa2a104361963
M153 <wanchatgame@gmail.com>
2023-09-14 12:49:14 +0700

    Operators

---
+++ Operator.ts
@@ -0,0 +1,22 @@
+// let x = 5 + 4;
+// let y = 5 - 4;
+// let z = 5 * 4;
+// let a = 5 / 4;
+// console.log(x);
+// console.log(y);
+// console.log(z);
+// console.log(a);
+
+// y++;
+// y--;
+// console.log(y)
+
+let b = 10;
+let c = 5;
+let d = b -= c;
+console.log(d);
+
+//and
+console.log(true && false);
+//or
+console.log(true || false);
