3a9d5f4e1f7e3432f0e41a2becb4da95204c09a4
M153 <wanchatgame@gmail.com>
2023-09-14 13:10:54 +0700

    Loops

---
+++ Loop.ts
@@ -0,0 +1,16 @@
+let foods = ["Pizza", "Burger", "Fries"];
+
+for (let item of foods) {
+    console.log(item);
+}
+
+for (let index of foods) {
+    console.log(foods[index]);
+}
+
+// let i = 0;
+// do 
+// {
+
+// }
+// while(true);
